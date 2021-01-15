c724d4050d4c82dee043e857587528dd307918bb
jyand <jyand@protonmail.com>
2021-01-15 06:16:29 +0000

    wordmatch

---
+++ wordtest.go
@@ -0,0 +1,52 @@
+package main
+import (
+        "fmt"
+        "os"
+        "bufio"
+        //"regexp"
+)
+
+func StringsFromFile(fpath string) []string {
+        f, _ := os.Open(fpath)
+        defer f.Close()
+
+        var s []string
+        sc := bufio.NewScanner(f)
+        for sc.Scan() {
+                s = append(s, sc.Text())
+        }
+
+        return s
+}
+
+func CharTuples() []string {
+        a := [26]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+        s := []string{}
+        for j := 0 ; j < len(a) - 1 ; j++ {
+                for i := j + 1 ; i < len(a) ; i++ {
+                        s = append(s, a[j] + a[i], a[i] + a[j])
+                }
+        }
+
+        return s
+}
+
+/*func MathTuples(str []string) []string {
+        words := StringsFromFile("corncob.dat")
+        s := []string{}
+}*/
+
+func main() {
+        words := StringsFromFile("corncob.dat")
+        for i := 0 ; i < len(words) ; i++ {
+                fmt.Println(words[i])
+        }
+        fmt.Println(len(words))
+
+        tuples := CharTuples()
+        for i := 0 ; i < len(tuples) ; i++ {
+                fmt.Println(tuples[i])
+        }
+        fmt.Println(len(tuples))
+}
