d45a6ce28ddf0699264d90a0be86a78c3a2e270b
jyand <jyand@protonmail.com>
2021-01-17 20:18:06 +0000

    added Webster

--- pmatch.go
+++ pmatch.go
@@ -53,7 +53,7 @@ func MatchTuples(tuples []string, words []string) []string {
 
 func main() {
         a := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-        str := MatchTuples(CharTuples(a, 1), StringsFromFile("corncob.dat"))
+        str := MatchTuples(CharTuples(a, 1), StringsFromFile("dict/corncob.dat"))
         for i := 0 ; i < len(str) ; i++ {
                 fmt.Println(str[i])
         }
